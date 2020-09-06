1ad32657b1540090bf6f98f6c2ffd3679ba26f92
Herman <herman@laxfors.com>
2020-09-07 08:29:03 +1000

    Wunderground API integrated

--- docs/main.js
+++ docs/main.js
@@ -10,3 +10,20 @@
       }
     } // for
   })()
+
+var GoogleMapsRequest = new XMLHttpRequest();
+GoogleMapsRequest.open("GET", "https://api.weather.com/v2/pws/observations/current?stationId=IMELBO1590&format=json&units=m&apiKey=6ed36b598d7440a8936b598d74c0a891", false);
+GoogleMapsRequest.responseType = 'json';
+GoogleMapsRequest.send();
+
+
+
+
+myvar = new XMLHttpRequest();
+myvar.open("GET","https://api.weather.com/v2/pws/observations/current?stationId=IMELBO1590&format=json&units=m&apiKey=6ed36b598d7440a8936b598d74c0a891");
+myvar.responseType = 'json';
+myvar.send()
+
+myvar.onload = function() {
+  document.getElementById("hermantest").innerHTML = myvar.response.observations[0].stationID;
+};
