856b7d55564329b1024fbbc960ce498d64ff0557
Herman <herman@laxfors.com>
2020-09-08 00:08:32 +1000

    Weather info table
    Added weather info to a table
    Added work in progress castle/weather graphic for potential future weather graphic
    Removed both liemannens, fire, and skull graphic

--- docs/main.js
+++ docs/main.js
@@ -11,19 +11,24 @@
     } // for
   })()
 
-var GoogleMapsRequest = new XMLHttpRequest();
-GoogleMapsRequest.open("GET", "https://api.weather.com/v2/pws/observations/current?stationId=IMELBO1590&format=json&units=m&apiKey=6ed36b598d7440a8936b598d74c0a891", false);
-GoogleMapsRequest.responseType = 'json';
-GoogleMapsRequest.send();
+// var GoogleMapsRequest = new XMLHttpRequest();
+// GoogleMapsRequest.open("GET", "https://api.weather.com/v2/pws/observations/current?stationId=IMELBO1590&format=json&units=m&apiKey=6ed36b598d7440a8936b598d74c0a891", false);
+// GoogleMapsRequest.responseType = 'json';
+// GoogleMapsRequest.send();
 
-
-
-
+// API get JSON
 myvar = new XMLHttpRequest();
 myvar.open("GET","https://api.weather.com/v2/pws/observations/current?stationId=IMELBO1590&format=json&units=m&apiKey=6ed36b598d7440a8936b598d74c0a891");
 myvar.responseType = 'json';
 myvar.send()
 
+// Data manipulation
+// Calculates feels like temperature depending certian factors
+
+
+// Enter data into table
 myvar.onload = function() {
-  document.getElementById("hermantest").innerHTML = myvar.response.observations[0].stationID;
+  document.getElementById("stationIDCell").innerHTML = myvar.response.observations[0].stationID;
+  document.getElementById("tempCell").innerHTML = myvar.response.observations[0].metric.temp;
+  document.getElementById("UVCell").innerHTML = myvar.response.observations[0].uv;
 };
