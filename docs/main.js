8c6b83d6a11aa8e12fe98ab214788c294b301058
Herman <herman@laxfors.com>
2020-09-08 04:01:37 +1000

    getWeather every 5 min
    added getWeather function
    setInterval now gets weather every 5 min
    added wind dir and speed

--- docs/main.js
+++ docs/main.js
@@ -16,19 +16,39 @@
 // GoogleMapsRequest.responseType = 'json';
 // GoogleMapsRequest.send();
 
-// API get JSON
-myvar = new XMLHttpRequest();
-myvar.open("GET","https://api.weather.com/v2/pws/observations/current?stationId=IMELBO1590&format=json&units=m&apiKey=6ed36b598d7440a8936b598d74c0a891");
-myvar.responseType = 'json';
-myvar.send()
+function getWeather() {
+  // API get JSON
+  weatherAPI = new XMLHttpRequest();
+  weatherAPI.open("GET","https://api.weather.com/v2/pws/observations/current?stationId=IMELBO1590&format=json&units=m&apiKey=6ed36b598d7440a8936b598d74c0a891");
+  weatherAPI.responseType = 'json';
+  weatherAPI.send()
 
-// Data manipulation
-// Calculates feels like temperature depending certian factors
+  // Wait for server response
+  weatherAPI.onload = function() {
+    
+    // Data manipulation
+    // Calculates feels like temperature depending certian factors
 
+    // Calculates human-readable wind directions from degrees
+    var windDeg = weatherAPI.response.observations[0].winddir;
+    var windCompass;
+    if (windDeg <= 45 && windDeg >= 0 || windDeg > 315) {
+      windCompass = "N";
+    } else if (windDeg <= 135 && windDeg > 45) {
+      windCompass = "W";
+    } else if (windDeg <= 225 && windDeg > 135) {
+      windCompass = "S";
+    } else if (windDeg <= 315 && windDeg > 225) {
+      windCompass = "E";
+    }
 
-// Enter data into table
-myvar.onload = function() {
-  document.getElementById("stationIDCell").innerHTML = myvar.response.observations[0].stationID;
-  document.getElementById("tempCell").innerHTML = myvar.response.observations[0].metric.temp;
-  document.getElementById("UVCell").innerHTML = myvar.response.observations[0].uv;
+    // Enter data into table
+    document.getElementById("stationIDCell").innerHTML = weatherAPI.response.observations[0].stationID;
+    document.getElementById("tempCell").innerHTML = weatherAPI.response.observations[0].metric.temp;
+    document.getElementById("UVCell").innerHTML = weatherAPI.response.observations[0].uv;
+    document.getElementById("windSpeedCell").innerHTML = weatherAPI.response.observations[0].metric.windSpeed;
+    document.getElementById("windDirCell").innerHTML = windCompass + "(" + weatherAPI.response.observations[0].winddir + "°)";
+  };
 };
+getWeather();
+setInterval(getWeather(),300000);
